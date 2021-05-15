e6485ba41422e731608758954562f21792748ae7
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:59:03 -0300

    Update Classe1.java

--- JAVA2/Classe1.java
+++ JAVA2/Classe1.java
@@ -9,10 +9,10 @@ public class Classe1
  
 public static void main (String [] args ){
     
- Classe1 ob1; // criando o objeto
- ob1 = new Classe1( ); // instanciamento de ob1 com new
+ Classe1 ob1; 
+ ob1 = new Classe1( ); 
  ob1.listar();
- Classe1 ob2 = new Classe1( ); // instanciamento de ob2 com new
+ Classe1 ob2 = new Classe1( ); 
  System.out.println( ob1.x + ob2.x );
 }
 }
