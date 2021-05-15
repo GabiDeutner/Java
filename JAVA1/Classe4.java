a7d898169303cadb49e9da6d28d7ce129cf97274
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:58:57 -0300

    Update Classe4.java

--- JAVA1/Classe4.java
+++ JAVA1/Classe4.java
@@ -1,11 +1,3 @@
-/*
-CLASSE 4 Divisibilidade Faça um algoritmo contendo uma function para
-armazenar o somatório de todos os valores entre n1 e n2, que são divisíveis por 3 e
-por 4 ao mesmo tempo e um VOID para mostrar o valor na tela. (faça a classe,
-instancie, indente o código, crie um leitor para ler n1 e n2, execute no main() com
-menu switch case, complete o código para rodar no IDE, peça ao usuário que digite o
-valor de n1 e de n2.
- */
 package AulaFor;
 
 public class Classe4 {
@@ -24,8 +16,5 @@ int result = soma(n1,n2);
 System.out.println("Somatorio de " + n1 + " ate "+n2+" = " + result); }
 
 
-/*void main ( String args [] ){
- int n1=15, n2 = 2148;
- mostrar ( n1 , n2 ); }  */
 
 }
