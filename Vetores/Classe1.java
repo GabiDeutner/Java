e1d7f847c8a6b0e5abc2e2416b40239ffe6e223e
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 23:01:30 -0300

    Update Classe1.java

--- Vetores/Classe1.java
+++ Vetores/Classe1.java
@@ -1,16 +1,9 @@
-/*
-Este exemplo cria o vetor x[] de forma explícita, em seguida
-descobre o número de elementos (n), x.length é o tamanho total do
-array x. Depois, é criada uma variável aux para armazenar a
-soma do quadrado do elemento do array com ele mesmo. O loop for
-é utilizado para armazenar e exibir o valor de aux. 
- */
 package AulaVetores;
 
 public class Classe1 {
 public static void main(String []agrs)
 { int x [] = { 1, 0 , -1 };
-int n = x.length;// tamaho
+int n = x.length;
  double aux = 0;
 for (int i = 0; i < n ; i ++)
  { aux = Math.pow( x[i], 2) + x[i];
