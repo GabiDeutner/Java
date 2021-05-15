1d7ff60d87699234da9e75f1a83b2f2c279c305a
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 23:01:38 -0300

    Update Classe2.java

--- Vetores/Classe2.java
+++ Vetores/Classe2.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package AulaVetores;
 
 import java.util.Scanner;
@@ -18,7 +13,7 @@ for (int i = 0; i < n ; i ++) {
 System.out.println("\nDigite salário e idade separados por espaço:");
  sal[i] = leitor.nextDouble();
  ida[i] = leitor.nextInt(); }
-// impriminto o vetor
+
 for ( int i=0; i<n; i++){ aux = sal[i] * 1.1;
 System.out.println( aux + " - " + ida[i]); }
 System.exit(0);}
