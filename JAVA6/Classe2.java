ed3fbffe6e1220517b271c5d9dde4c41ff9cb0f7
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 23:01:02 -0300

    Update Classe2.java

--- JAVA6/Classe2.java
+++ JAVA6/Classe2.java
@@ -3,19 +3,18 @@ package aulaWhile;
 
 
 public class Classe2 {
-//procurar todos os valores pares entre o início e o fim e somar todos
-// inicio é 10 e o fim é 20, procura os pares entre 10 e 20
-int somapar ( int inicio, int fim ) { //dois valores, um de inicio e um de fim
+
+int somapar ( int inicio, int fim ) { 
  int somapares =0;
  
- while ( inicio <= fim ) { //enquanto o início for menor ou igual ao fim
- if ( inicio % 2 == 0 ) // para ver se o valor é par
+ while ( inicio <= fim ) { 
+ if ( inicio % 2 == 0 ) 
  { somapares += inicio; }
  inicio ++; }
 return somapares ; }
 
 void executar (int a, int b) {
-    int pares = somapar (a, b); // recebe a=10 e b=20
+    int pares = somapar (a, b); 
 
  System.out.println("\nSoma de pares:"+pares); 
 }
@@ -23,6 +22,6 @@ void executar (int a, int b) {
 public static void main(String []agrs){
     
     Classe2 m = new Classe2 (); 
-    m.executar(10, 20); // inicio vai ser 10 e final vai ser 20
+    m.executar(10, 20); 
 } 
 }
