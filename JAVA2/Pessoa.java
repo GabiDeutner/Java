fcce851b215a035e42d59a4fccf8b5a138e7a4d2
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:59:18 -0300

    Update Pessoa.java

--- JAVA2/Pessoa.java
+++ JAVA2/Pessoa.java
@@ -1,33 +1,19 @@
-/*
-1) Faça o código fonte de uma classe que contenha atributos pessoais de uma pessoa:
-nome, idade, sexo, telefone, endereço e e-mail. Nesta classe crie um construtor com
-parâmetros para inicializar os atributos da classe e crie também um método para exibir os
-atributos. 
 
-2) Depois faça a classe principal contendo um menu de controle, o método main() e um
-método para ler cada atributo pelo teclado. Observem que a classe Pessoa deverá ser
-instanciada somente quando a opção de leitura de menu for selecionada, já que vcs irão
-utilizar um construtor com parametros para enviar os atributos lidos para dentro da classe
-Pessoa. 
-
- */
 package AulaJava5;
 
 import java.util.Scanner;
 
 public class Pessoa {
   
-    /**
-     *
-     */
-    public String [] dados; //= {"a","b","c","d","e","f"}; 
+   
+    public String [] dados; 
     
  
  public Pessoa(int capacidade){
        this.dados = new String[capacidade];
     }
 
- // métodos de leitura dos atributos
+
 public void ler (Scanner leitor){  
     
 this.dados[0] = leitor.nextLine();  
@@ -39,7 +25,7 @@ this.dados[5] = leitor.nextLine();
     }
 
 
-Pessoa ( ) { } // construtor vazio
+Pessoa ( ) { } 
  
 Pessoa ( String [] tb ) {
 for ( String valor: tb )
