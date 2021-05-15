79fb56021011c5b4ab40abb8a3bcfb63fe6c8e1b
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 21:56:27 -0300

    Create Classe3.java

---
+++ Vetores/Classe3.java
@@ -0,0 +1,19 @@
+
+package AulaVetores;
+
+import java.util.Scanner;
+
+
+public class Classe3 {
+public static void main (String [] args) {  
+Scanner leitor = new Scanner(System.in);
+String letra, frase;
+System.out.println("\nDigite a frase:");
+frase = leitor.nextLine();// ler um texto
+int n = frase.length(); // tamanho do string
+for (int i = 0; i < n ; i ++) {
+ letra = frase.substring (i, i+1);
+ System.out.println("\n"+letra); }
+ System.out.println("\nA frase tem "+ n+"caracteres..!\n"); System.exit(0); }
+ 
+}
