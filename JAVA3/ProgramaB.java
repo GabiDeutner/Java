1b94373cfb64e85b6d3d854183f1b3cf57523faf
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:59:40 -0300

    Update ProgramaB.java

--- JAVA3/ProgramaB.java
+++ JAVA3/ProgramaB.java
@@ -4,19 +4,19 @@ package ExercicioJava;
 public class ProgramaB {
 
     public double SM, PR;
-    
-// métodos de leitura dos atributos
+
+
 void lerSM ( double SM1) { this.SM = SM1; }
 void lerPR ( double PR1) { this.PR = PR1; }
 
-// métodos para calcular os resultados
+
 
 double calculaNS () {
     double NS = (this.SM)*(1+(this.PR/100));
 return NS;
 }
 
-// método de exibição dos resultados
+
 public void exibir ( ) {
  double NS = calculaNS();
 
