421228e6adcabd3f38f7489d85532947cde3ae37
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:59:47 -0300

    Update ProgramaC.java

--- JAVA3/ProgramaC.java
+++ JAVA3/ProgramaC.java
@@ -3,24 +3,22 @@ package ExercicioJava;
 
 public class ProgramaC {
     
-// atributos de leitura
+
 int va, vb, vc, vn, vbr;    
 
-// métodos de leitura dos atributos
+
 void ler_A ( int a ) { this.va = a; }
 void ler_B ( int b ) { this.vb = b; }
 void ler_C ( int c ) { this.vc = c; }
 void ler_nulos ( int n ) { this.vn = n; }
 void ler_brancos ( int br ) { this.vbr = br; }
 
-// métodos para calcular os resultados
 
-//calcula numero total de eleitores
 int calculaTotal () {
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 return T;}
 
-//calcula percentual de votos válidos em relação ao total de eleitores
+
 double calculaP1 (){
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 double P = (this.va + this.vb + this.vc);
@@ -28,7 +26,7 @@ double P1 = (P/T)*100;
 return P1;
 }
 
-//calcula percentual de votos válidos do candidato A em relação ao total de eleitores
+
 double calculaP2 (){
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 double P = this.va;
@@ -36,7 +34,7 @@ double P2 = (P/T)*100;
 return P2;
 }
 
-// calcula percentual votos válidos do candidato B em relação ao total de eleitores
+
 double calculaP3 (){
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 double P = this.vb;
@@ -44,7 +42,7 @@ double P3 = (P/T)*100;
 return P3;
 }
 
-// calcula percentual votos válidos do candidato C em relação ao total de eleitores
+
 double calculaP4 (){
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 double P = this.vc;
@@ -52,7 +50,7 @@ double P4 = (P/T)*100;
 return P4;
 }
 
-// calcula percentual votos nulos em relação ao total de eleitores
+
 double calculaP5 (){
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 double P = this.vn;
@@ -60,7 +58,7 @@ double P5 = (P/T)*100;
 return P5;
 }
 
-// calcula percentual votos brancos em relação ao total de eleitores
+
 double calculaP6 (){
 int T = (this.va + this.vb + this.vc + this.vn + this.vbr );
 double P = this.vbr;
@@ -68,7 +66,7 @@ double P6 = (P/T)*100;
 return P6;
 }
 
-// método de exibição
+
 public void exibir(){
 int T = calculaTotal ();
 double P1 = calculaP1();
