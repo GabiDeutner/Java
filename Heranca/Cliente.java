576083c5bef5bc09d33ca3c0d577f5cb906044b5
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:58:18 -0300

    Update Cliente.java

--- Heranca/Cliente.java
+++ Heranca/Cliente.java
@@ -1,17 +1,17 @@
 
 package AulaHeranca;
 
-public class Cliente extends Object{ //classe mãe sempre herda a classe object
+public class Cliente extends Object{ 
 
 public String tipo;
 public String telefone;
 
- Cliente (String tp) { // classe construtora
- tipo = new String(); // instanciando tipo, isso inicializa o atributo
- this.tipo = tp; }      // o atributo tipo recebe tp
+ Cliente (String tp) { 
+ tipo = new String(); 
+ this.tipo = tp; }      
  
  public void exibir(){
- System.out.println(tipo); //tipo foi instanciada e recebeu tp, será exibida
+ System.out.println(tipo); 
  }
  
 }
