df3fee5a59e3634b105c553d9c93f1a1f976cdd0
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 22:58:28 -0300

    Update Empresa.java

--- Heranca/Empresa.java
+++ Heranca/Empresa.java
@@ -1,27 +1,27 @@
 
 package AulaHeranca;
 
-public class Empresa extends Cliente{ // a classe mãe é a classe Cliente
+public class Empresa extends Cliente{ 
     
  public String rua;
  
  
- Empresa( String tp, String rua ) { // método construtor
- super(tp); //recebe tp da classe mãe, o super traz da classe mãe
- this.rua = rua; //variavel local recebe variável global
+ Empresa( String tp, String rua ) { 
+ super(tp); 
+ this.rua = rua; 
  }
 
-public static void main (String [] args) { //método main para executar a classe
+public static void main (String [] args) { 
     
- String meutipo= "Pessoa Física"; //crio uma variável String que recebe essa frase
+ String meutipo= "Pessoa Física"; 
  String rua = "Rua A ";
  
- Empresa c1 = new Empresa ( meutipo, rua ); //instancio a classe com as variáveis criadas neste método
+ Empresa c1 = new Empresa ( meutipo, rua ); 
  
- c1.telefone = "11-955443322"; //objeto c1 recebe o telefone da classe mãe
+ c1.telefone = "11-955443322"; 
  c1.rua = c1.rua + ", número 9";
  
- System.out.println(c1.telefone); //herda telefone da classe mãe
+ System.out.println(c1.telefone); 
  System.out.println(c1.rua); c1.exibir();
  
  c1.tipo = "Pessoa Jurídica";
