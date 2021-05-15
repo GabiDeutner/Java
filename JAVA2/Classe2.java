895f212180c9c72398799769997ec9f0b2090b4b
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-05-14 21:45:21 -0300

    Create Classe2.java

---
+++ JAVA2/Classe2.java
@@ -0,0 +1,19 @@
+package AulaJava5;
+
+public class Classe2 {
+    
+ public int x [ ]={1,2,3,4,5};
+ 
+Classe2 ( ) { } // construtor vazio
+
+Classe2 ( int [] tb ) {
+for ( int valor: tb )
+ System.out.println(valor);
+}
+
+public static void main (String []args) {
+Classe2 c1 = new Classe2 ();
+Classe2 c2 = new Classe2 (c1.x); } 
+    
+    
+}
