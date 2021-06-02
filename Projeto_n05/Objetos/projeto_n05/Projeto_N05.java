46b8b74138beadc4e3318776d86bbce27a4cd347
Gabriela Paulini Deutner <45831598+GabiDeutner@users.noreply.github.com>
2021-06-02 00:08:32 -0300

    Create Projeto_N05.java

---
+++ Projeto_n05/Objetos/projeto_n05/Projeto_N05.java
@@ -0,0 +1,13 @@
+package projeto_n05;
+
+public class Projeto_N05 {
+
+    public static void main(String[] args) {
+
+        fr_Senha  localSenha = new fr_Senha ();
+        
+        localSenha.setVisible(true);
+        
+    }
+    
+}
